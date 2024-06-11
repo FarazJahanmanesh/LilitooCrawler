8f2fede1593b7ee80ebc3b29ca4250c129f43fb8
faraz jahan manesh <farazebipax@gmail.com>
2024-06-11 14:59:23 +0330

    add base action response

---
+++ Domain/Common/ActionResponse.cs
@@ -0,0 +1,35 @@
+﻿namespace Domain.Common;
+public class ActionResponse<T> : IActionResponse
+{
+    public ActionResponse()
+    {
+        Errors = new List<string>();
+    }
+
+    public int Status { get; set; }
+    public string Message { get; set; }
+    public T Data { get; set; }
+    public ResponseStateEnum State { get; set; }
+    public List<string> Errors { get; set; }
+}
+public class ActionResponse : IActionResponse
+{
+    public ActionResponse()
+    {
+        Errors = new List<string>();
+    }
+
+    public ResponseStateEnum State { get; set; }
+    public List<string> Errors { get; set; }
+}
+public interface IActionResponse
+{
+    ResponseStateEnum State { get; set; }
+    List<string> Errors { get; set; }
+}
+public enum ResponseStateEnum
+{
+    FAILED = 0,
+    SUCCESS = 1,
+    NOTAUTHORIZED = 2
+}
